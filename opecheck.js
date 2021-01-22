2c606fca7b285e02fc0ffa0a0710d059ee311dbd
hureta <40241920+0t3k25@users.noreply.github.com>
2021-01-23 03:14:16 +0900

    change

--- opecheck.js
+++ opecheck.js
@@ -10,13 +10,8 @@ console.log("接続成功");
 
 //データベースよりcarousel_imageを取得
 app.get("/", cors(), (req, res) => {
-    if (err) {
-        console.log("失敗だよーーーーーーーーーーーーーん");
-        console.log(err);
-    } else {
-        //console.log(client.rows);
-        res.send("hello world");
-    }
+    //console.log(client.rows);
+    res.send("hello world");
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
