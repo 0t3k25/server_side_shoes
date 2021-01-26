199b922c0b134c9b8721340f4f8b4911340df9f1
hureta <40241920+0t3k25@users.noreply.github.com>
2021-01-26 22:49:43 +0900

    change json

--- opecheck.js
+++ opecheck.js
@@ -11,7 +11,7 @@ console.log("接続成功");
 //データベースよりcarousel_imageを取得
 app.get("/hello", cors(), (req, res) => {
     //console.log(client.rows);
-    res.send("hello world");
+    res.send(["hello world"]);
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
