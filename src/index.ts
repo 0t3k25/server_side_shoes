e0dff6906ff035b54d52cf5b5b706aaaf64e6a25
hureta <40241920+0t3k25@users.noreply.github.com>
2021-02-15 01:26:40 +0900

    add cors

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { ShoeController } from './controller/ShoeController'
 createConnection()
   .then(async () => {
     createExpressServer({
+      cors: true,
       controllers: [ShoeController],
     }).listen(3000)
     console.log('Server is up and running on port 3000.')
